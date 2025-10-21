158071b2de640408175dd89c2b505a370bc1b0c5
unknown <mounika@gsspec.com>
2025-10-21 19:17:08 +0530

    company data reset

---
+++ scripts.sql
@@ -0,0 +1,39 @@
+SET @company_id = 1;
+
+
+DELETE FROM ivfeedback
+WHERE candidateid IN (SELECT id FROM candidate WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id));
+
+DELETE FROM interviewmedia
+WHERE candidateid IN (SELECT id FROM candidate WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id));
+
+DELETE FROM callschedule
+WHERE candidateid IN (SELECT id FROM candidate WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id));
+
+DELETE FROM qresponse
+WHERE candidateid IN (SELECT id FROM candidate WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id));
+
+DELETE FROM registration
+WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id);
+
+DELETE FROM candidate
+WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id);
+
+DELETE FROM jdanalysis
+WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id);
+
+DELETE FROM brules
+WHERE jobdescid IN (SELECT id FROM jobdesc WHERE companyid = @company_id);
+
+DELETE FROM workflow
+WHERE jobid IN (SELECT id FROM jobdesc WHERE companyid = @company_id);
+
+DELETE FROM jobdesc
+WHERE companyid = @company_id;
+
+DELETE FROM credits
+WHERE  companyid = @company_id;
+
+
+
+
